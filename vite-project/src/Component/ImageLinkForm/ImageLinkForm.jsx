5319babb65d71020ac2c08806e2ec6075e1e99b5
Terre8055 <michaelappiah2018@icloud.com>
2023-02-03 14:27:53 +0000

    Added Placeholder

--- vite-project/src/Component/ImageLinkForm/ImageLinkForm.jsx
+++ vite-project/src/Component/ImageLinkForm/ImageLinkForm.jsx
@@ -69,7 +69,7 @@ export default function ImageLinkForm() {
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input type="text" className="f4 pa2 w-70 center" onChange={onInputChange} />
+                    <input placeholder="Paste image link" type="text" className="f4 pa2 w-70 center" onChange={onInputChange} />
                     <button type="submit"  className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonSubmit}>
                         Detect
                     </button> 
